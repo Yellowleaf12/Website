52696a548607774c3dd448542e40a7ec9fe01161
Yellowleaf12 <112094595+Yellowleaf12@users.noreply.github.com>
2022-08-25 21:42:42 -0300

    Add files via upload

---
+++ js/formulario-check.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    var show = document.getElementById('show');
+    if(show.type=='password'){
+        show.type='text';
+    }
+    else{
+        show.type='password';
+    }
+}
